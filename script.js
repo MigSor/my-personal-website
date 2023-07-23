a6a3e177db1b0bb48afc4bf47e25228bf5976724
Miguel Soriano <miguel.sor.88@gmail.com>
2023-07-23 11:54:59 +1000

    added search anime

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
-// document.querySelector("body").appendChild(h2);
-// const h2 = document.createElement("h2");
-// h2.textContent = "This content added by JavaScript";
+let animeForm = document.getElementById("animeForm");
+let animeInput = document.getElementById("animeInput");
+let nav = document.querySelector(".nav");
+let menu = document.querySelector(".menu");
+let searchResult = document.querySelector("#searchResult");
+
+async function fetchAnime(pokemonName = "Pokemon") {
+  await fetch(`https://api.jikan.moe/v4/anime?q=${pokemonName}`)
+    .then((response) => response.json())
+    .then((results) => {
+      // console.log(results.data);
+      let animeList = results.data;
+
+      for (let item of animeList) {
+        let li = document.createElement("li");
+        let img = document.createElement("img");
+        let a = document.createElement("a");
+        console.log(item.url);
+        img.src = item.images.jpg.image_url;
+        li.textContent = item.title;
+        li.append(img);
+        searchResult.append(li);
+        // console.log(item.title);
+        // console.log(item.images.jpg.image_url);
+      }
+    });
+}
+
+animeForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  fetchAnime(animeInput.value);
+  animeInput.value = "";
+});
+
+window.addEventListener("resize", function (event) {
+  let newWidth = window.innerWidth;
+  if (newWidth <= 900) {
+    nav.classList.add("invi");
+    menu.classList.remove("invi");
+  } else {
+    nav.classList.remove("invi");
+    menu.classList.add("invi");
+  }
+});
+
+menu.addEventListener("click", () => {
+  nav.classList.toggle("nav-small");
+});
